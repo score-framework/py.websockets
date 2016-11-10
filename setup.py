bdcf85601c5ca27836e99dfbfc1735b51a4bc8f7
Necdet Can Ateşman <c.atesman@gmx.net>
2016-11-10 11:51:04 +0100

    Fix: Package version is 0.0.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.ws',
-    version='0.2.4',
+    version='0.0.1',
     description='HTTP handler of The SCORE Framework',
     long_description=README,
     author='strg.at',
