8456e32c6f54de7edc8327a4d060e3943bbffbf4
Necdet Can Ateşman <can.atesman@strg.at>
2018-01-10 16:59:57 +0100

    Release: 0.1.0

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright © 2015-2017 STRG.AT GmbH, Vienna, Austria
+# Copyright © 2015-2018 STRG.AT GmbH, Vienna, Austria
 #
 # This file is part of the The SCORE Framework.
 #
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.websockets',
-    version='0.0.8',
+    version='0.1.0',
     description='Websockets integration for The SCORE Framework',
     long_description=README,
     author='strg.at',
